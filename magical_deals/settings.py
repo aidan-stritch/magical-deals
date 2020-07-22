98313d4b3788d8e75f2fca16a90478b17ff86f88
aidan-stritch <aidan.stritch@gmail.com>
2020-07-22 00:01:20 +0000

    added stripe functionality to manage credit card payments at checkout

--- magical_deals/settings.py
+++ magical_deals/settings.py
@@ -138,4 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
