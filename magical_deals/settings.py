2e909180c25a39cb5345b99c1452f5d5f9440fd8
aidan-stritch <aidan.stritch@gmail.com>
2020-07-27 19:39:57 +0000

    working on images not loading on profile.html / changed settings.py media url

--- magical_deals/settings.py
+++ magical_deals/settings.py
@@ -164,8 +164,8 @@ MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-"""MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)"""
-MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+"""MEDIA_URL = '/media/'"""
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
