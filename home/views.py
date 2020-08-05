28ed73904b30ccdf6b0dba68425c71f17a53d60c
aidan-stritch <aidan.stritch@gmail.com>
2020-08-05 20:27:07 +0000

    added section to index.html showcasing a small sample of recently bought items to entice non members

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from products.models import Product
-
+from checkout.models import Order, OrderLineItem
 
 # Create your views here.
 
@@ -8,13 +8,13 @@ from products.models import Product
 def index(request):
     """displays the index page"""
     products = Product.objects.all()
-    return render(request, "index.html", {"products": products})
+    orders = Order.objects.all()
+    items = OrderLineItem.objects.order_by('product_id').distinct('product_id')
+    args = {"products": products, "orders": orders, "items": items}
+    return render(request, "index.html", args)
 
 
 def about(request):
     """displays the about us page"""
 
     return render(request, "about.html")
-
-
-
