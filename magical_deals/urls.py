b1c905a2072d72322fe1e1fd7ef74595d09c059d
aidan-stritch <aidan.stritch@gmail.com>
2020-08-28 18:53:36 +0000

    added 404 url

--- magical_deals/urls.py
+++ magical_deals/urls.py
@@ -11,6 +11,7 @@ from checkout import urls as urls_checkout
 from search import urls as urls_search
 from django.views import static
 from .settings import MEDIA_ROOT
+from home.views import index
 
 urlpatterns = [
     url(r'^', include(urls_home)),
@@ -22,4 +23,5 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
+    url(r'^.*/$', index, name='404')
 ]
